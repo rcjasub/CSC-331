05b86118c0778578a5edda9f91f5ec9bb76aa77c
Jocsan Rodriguez <jasub270@gmail.com>
2025-09-01 21:09:34 -0400

    finished the .h file for linklistType

--- Assigment 1/linkedListIterator.h
+++ Assigment 1/linkedListIterator.h
@@ -1,35 +1,34 @@
+#pragma once
+#include "nodeType.h"
+
 template <class type>
 class linkedListIterator
 {
 public:
+    linkedListIterator();
+    // Default constructor
+    // Postcondition: current is set to nullptr
 
-linkedListIterator();
-//Default constructor
-//postcondition: current is set to nullptr
+    linkedListIterator(nodeType<type> *ptr);
+    // Constructor with parameter
+    // Postcondition: current is set to ptr
 
-linkedListIterator(nodeType<type>* ptr);
-//Constructor with parameter
-//Postcondition: current is set to ptr
+    type operator*();
+    // Function to overload the dereferencing operator *
+    // Postcondition: Returns the info contained in the node
 
-type operator*();
-//Function to overload the dereferencing operator *
-//Postcondition: Returns the info contained in the node
+    linkedListIterator<type> operator++();
+    // Overload the pre-increment operator
+    // Postcondition: The iterator is advanced to the next node
 
-linkedListIterator<type> operator++();
-//Overload the pre-increment operator
-//Postcondition: The iterator is advanced to the next node
+    bool operator==(const linkedListIterator<type> &right) const;
+    // Overload the equality operator
+    // Postcondition: Returns true if this iterator is equal to the right iterator, otherwise it returns false
 
-bool operator==(const linkedListIterator<type>& right) const;
-//Overload the equality operator
-//Postcondition: Returns true if this iterator is equal to the right iterator, otherwise it returns false
-
-bool operator!=(const linkedListIterator<type>& right) const;
-//Overload the not equal to operator
-//Postcondition: Returns true if this iterator is not equal to the right iterator, otherwise it returns false
+    bool operator!=(const linkedListIterator<type> &right) const;
+    // Overload the not equal to operator
+    // Postcondition: Returns true if this iterator is not equal to the right iterator, otherwise it returns false
 
 private:
-nodeType<type>* current; //pointer to point to the current node in the linked list
-
+    nodeType<type> *current; // pointer to point to the current node in the linked list
 };
-
-
---
+++ Assigment 1/linkedListType.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "linkedListType.h"
+
+template <class Type>
+inline const LinkListType<Type> &LinkListType<Type>::operator=(const LinkListType<Type> &other)
+{
+    // TODO: insert return statement here
+}
+
+template <class Type>
+void LinkListType<Type>::initiallizeList()
+{
+}
+
+template <class Type>
+bool LinkListType<Type>::isEmptyList() const
+{
+    return false;
+}
+
+template <class Type>
+void LinkListType<Type>::print() const
+{
+}
+
+template <class Type>
+void LinkListType<Type>::length() const
+{
+}
+
+template <class Type>
+void LinkListType<Type>::destroyList()
+{
+}
+
+template <class Type>
+Type LinkListType<Type>::front() const
+{
+    return Type();
+}
+
+template <class Type>
+Type LinkListType<Type>::back() const
+{
+    return Type();
+}
